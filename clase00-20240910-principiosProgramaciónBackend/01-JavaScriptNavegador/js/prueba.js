9a27e9c35de029affb93f96625de1f980c7eb399
Diego Polverelli <diegopolverelli@hotmail.com>
2024-09-10 21:19:44 -0300

    clase 00 added

---
+++ clase00-20240910-principiosProgramaciónBackend/01-JavaScriptNavegador/js/prueba.js
@@ -0,0 +1,7 @@
+console.log("hola...!!!")
+let usuarios=[
+    {id:1, nombre:"Luciana", email:"luciana@test.com", password:"123", rol:"user"},
+    {id:2, nombre:"Juan", email:"juan@test.com", password:"123", rol:"user"},
+    {id:3, nombre:"Romina", email:"romina@test.com", password:"123", rol:"admin"},
+]
+usuarios.forEach(u=>console.log(u))
